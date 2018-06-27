dfb6ce96939c6779a42cc562f45c413c08d8c0e2
zzq <2455676683@qq.com>
2018-06-27 18:18:02 +0800

    yoyiyi

---
+++ src/main/java/com/yoyiyi/gradle/domain/City.java
@@ -0,0 +1,63 @@
+package com.yoyiyi.gradle.domain;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * 类名：City
+ * 描述：TODO
+ * 时间：2018/6/27 17:40
+ *
+ * @author <a href="2455676683@qq.com">zzq</a>
+ * @version 1.0
+ */
+@XmlRootElement(name = "d")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class City {
+    @XmlAttribute(name = "d1")
+    private String cityId;
+
+    @XmlAttribute(name = "d2")
+    private String cityName;
+
+    @XmlAttribute(name = "d3")
+    private String cityCode;
+
+    @XmlAttribute(name = "d4")
+    private String province;
+
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+}
