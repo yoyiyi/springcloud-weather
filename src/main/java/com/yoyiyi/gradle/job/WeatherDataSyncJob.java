90b8299d73ea161de8c56385e2b719980bb89e3d
zzq <2455676683@qq.com>
2018-06-30 10:34:11 +0800

    yoyiyi

--- src/main/java/com/yoyiyi/gradle/job/WeatherDataSyncJob.java
+++ src/main/java/com/yoyiyi/gradle/job/WeatherDataSyncJob.java
@@ -1,8 +1,15 @@
 package com.yoyiyi.gradle.job;
 
+import com.yoyiyi.gradle.domain.City;
+import com.yoyiyi.gradle.service.ICityDataService;
+import com.yoyiyi.gradle.service.IWeatherDataService;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.List;
 
 /**
  * 类名：WeatherDataSyncJob
@@ -13,10 +20,24 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
  * @version 1.0
  */
 public class WeatherDataSyncJob extends QuartzJobBean {
-
+    @Autowired
+    private ICityDataService mCityDataService;
+    @Autowired
+    private IWeatherDataService mWeatherDataService;
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-
+        //遍历城市id列表
+        List<City> cityList = null;
+        try {
+            cityList = mCityDataService.listCity();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //遍历城市id获取天气
+        for (City city : cityList) {
+            String cityId = city.getCityId();
+            mWeatherDataService.syncDataByCityId(cityId);
+        }
     }
 }
